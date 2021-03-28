2c769540a5ea4c5c25f1e67e27e93fbac0a823f7
S3cBar0n <secberg@gmail.com>
2021-03-27 19:43:15 -0600

    Fixed unhandled error on line 21

--- main.go
+++ main.go
@@ -11,14 +11,18 @@ func netWorker(ports, results chan int, address string) {
 	// Worker receives up to 100 ports and for the ports in that range it attempts to scan each port in the range
 	for p := range ports {
 		address := fmt.Sprintf("%v:%d", address, p)
-		conn, err := net.Dial("tcp",  address)
+		conn, err := net.Dial("tcp", address)
 		// If connection fails it sends a 0 to the results channel to be filtered out
 		if err != nil {
 			results <- 0
 			continue
 		}
 		// If successful it closes connection and sends port to the results channel
-		conn.Close()
+		err2 := conn.Close()
+		if err2 != nil {
+			log.Fatal("Unable to close connection... Quiting...")
+		}
+
 		results <- p
 	}
 }
@@ -32,7 +36,6 @@ func main() {
 		log.Fatal("A valid web address was not entered...")
 	}
 	fmt.Print("Scanning... Please Wait...\n")
-
 
 	// Creating 2 channels, 1 to send ports to our worker and the other to store the results the workers gather
 	ports := make(chan int, 100)
@@ -54,7 +57,7 @@ func main() {
 
 	// The results the worker sends to the results channel is processed here and stored in the open ports variable
 	for i := 0; i < 1024; i++ {
-		port := <- results
+		port := <-results
 		if port != 0 {
 			openports = append(openports, port)
 		}
